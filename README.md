da1182ce34228b7c906952bb7c2ecc7b92612a83
jannis2301 <jannis.ax@gmx.de>
2022-04-18 22:21:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# **Random Color Generator**
+
+Random Color Generator is an app built with JavaScript, to help people find new color schemes.
+This training project is prepared by [Codecademy](https://www.codecademy.com).
+
+## Table of Contents
+
+- [Project Prompt](#project-prompt)
+- [Technologies](#technologies)
+- [Sources](#sources)
+
+## Project Prompt
+
+In this project we built an app with JavaScript to help people find new color schemes. You can click on the 'Pick a Color' button which will refresh to a new, random color or rotate the mouse wheel and get the 'Mystery Color'.
+
+## Technologies
+
+- HTML
+- CSS
+- JavaScript ES6
+
+## Sources
+
+The techniques utilized was based on the lessons taught in [Codecademy's Building Interactive Websites](https://www.codecademy.com/learn/build-interactive-websites). The challenge is also provided by Codecademy.
---
+++ resources/js/main.js
@@ -0,0 +1,27 @@
+// This variable stores the "Pick a Color" button
+let button = document.getElementById('color-button');
+
+// This variable stores the "Mystery Color" button
+let mysteryButton = document.getElementById('next-button');
+
+// This random number function will create color codes for the randomColor variable
+function colorValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+// Event handler function, to randomly change the colors of the buttons when specific events are fired on them.
+function colorChange(event){
+  let colorValue1 = colorValue();
+  let colorValue2 = colorValue();
+  let colorValue3 = colorValue();
+  let randomColor = 'rgb(' + colorValue1 + ', ' + colorValue2 + ', ' + colorValue3 + ')';
+  event.target.style.backgroundColor = randomColor;
+  event.target.innerHTML = `Your color is ${randomColor}`;
+  const lightColor = colorValue1 + colorValue2 + colorValue3;
+  lightColor < 381 ? event.target.style.color = '#000': event.target.style.color = '#fff';
+}
+
+// An event handler property on the button element that fires when it’s clicked. colorChange() is the event handler function.
+button.addEventListener('click', colorChange);
+// An event handler property on the mysteryButton element, that fires when you rotate the mouse wheel or slide down on the mousepad
+mysteryButton.addEventListener('wheel', colorChange);
